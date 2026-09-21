d8e8dc6d308e2cfd1176deeb98e1ed1e83b51e95
damirmoskov <moskov.daamiir@yandex.com>
2026-09-21 18:54:46 +0000

    1

--- vocabulary/ka/A1/speaking.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const lang = "ka";
-    if (!window.speakingData) window.speakingData = {};
-    if (!window.speakingData[lang]) window.speakingData[lang] = {};
-    const keys = ["talkThatTalk", "debates", "opinions", "quotes", "fluency"];
-    keys.forEach(k => {
-        if (!window.speakingData[lang][k]) window.speakingData[lang][k] = [];
-    });
-})();
