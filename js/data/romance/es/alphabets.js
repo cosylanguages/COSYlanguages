f2273c39e6e3a985f2cb6982f8a8cbe3e2c37deb
cosylanguages <cosylanguages@gmail.com>
2026-09-26 20:43:01 +0200

    Delete js/data/romance/es/alphabets.js

--- js/data/romance/es/alphabets.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    const data = "abcdefghijklmnñopqrstuvwxyz";
-    const lang = "es";
-    window.alphabetsData = window.alphabetsData || {};
-    window.alphabetsData[lang] = data;
-})();
