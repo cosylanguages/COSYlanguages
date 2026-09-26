f38d64ac7b5e946d8afac5a13bd35208df33cf0d
cosylanguages <cosylanguages@gmail.com>
2026-09-26 20:42:25 +0200

    Delete js/data/celtic/br/alphabets.js

--- js/data/celtic/br/alphabets.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    const data = "abcdefghijklmnoprstuvwyz";
-    const lang = "br";
-    window.alphabetsData = window.alphabetsData || {};
-    window.alphabetsData[lang] = data;
-})();
