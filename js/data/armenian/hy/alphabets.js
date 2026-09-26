502b4ee6db4e8cb87557cf409b39ab8db7356061
cosylanguages <cosylanguages@gmail.com>
2026-09-26 20:43:46 +0200

    Delete js/data/armenian/hy/alphabets.js

--- js/data/armenian/hy/alphabets.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    const data = "աբգդեզէըթժիլխծկհձղճմյնշոչպջռսվտրցւփքօֆև";
-    const lang = "hy";
-    window.alphabetsData = window.alphabetsData || {};
-    window.alphabetsData[lang] = data;
-})();
