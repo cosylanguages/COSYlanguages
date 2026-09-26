7a1ce6decf1bf3c13bbe805d7ac068813268008a
cosylanguages <cosylanguages@gmail.com>
2026-09-26 20:44:18 +0200

    Delete js/data/kartvelian/ka/alphabets.js

--- js/data/kartvelian/ka/alphabets.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    const data = "აბგდევზთიკლმნოპჟრსტუფქღყშჩცძწჭხჯჰ";
-    const lang = "ka";
-    window.alphabetsData = window.alphabetsData || {};
-    window.alphabetsData[lang] = data;
-})();
