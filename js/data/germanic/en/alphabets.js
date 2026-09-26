057d1f48bef02b5763a45760a78e05e2f75e03ec
cosylanguages <cosylanguages@gmail.com>
2026-09-26 20:44:02 +0200

    Delete js/data/germanic/en/alphabets.js

--- js/data/germanic/en/alphabets.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    const data = "abcdefghijklmnopqrstuvwxyz";
-    const lang = "en";
-    window.alphabetsData = window.alphabetsData || {};
-    window.alphabetsData[lang] = data;
-})();
