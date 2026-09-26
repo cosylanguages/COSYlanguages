d3cb313dd709814db5c038dee9f2ae8a98e55a0b
cosylanguages <cosylanguages@gmail.com>
2026-09-26 20:44:10 +0200

    Delete js/data/hellenic/el/alphabets.js

--- js/data/hellenic/el/alphabets.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    const data = "αβγδεζηθικλμνξοπρστυφχψω";
-    const lang = "el";
-    window.alphabetsData = window.alphabetsData || {};
-    window.alphabetsData[lang] = data;
-})();
