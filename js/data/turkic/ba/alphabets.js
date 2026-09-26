46411d18718c7970e65e8181a6225f0ea736466a
cosylanguages <cosylanguages@gmail.com>
2026-09-26 20:42:45 +0200

    Delete js/data/turkic/ba/alphabets.js

--- js/data/turkic/ba/alphabets.js
+++
@@ -1,6 +0,0 @@
-(function() {
-    const data = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяәғҙҡңөҫүһ";
-    const lang = "ba";
-    window.alphabetsData = window.alphabetsData || {};
-    window.alphabetsData[lang] = data;
-})();
